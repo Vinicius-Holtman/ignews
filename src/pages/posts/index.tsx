c2d11ec9df94831c5700627f5de2b29d9c133043
Vinicius <viniholtman28@gmail.com>
2022-04-19 14:42:00 -0300

    Map posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,24 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
+
+import Prismic from '@prismicio/client'
+import { getPrismicClient } from '../../services/prismic'
+import { RichText } from 'prismic-dom'
+
 import styles from './styles.module.scss'
 
-export default function Posts() {
+type Post = {
+	slug: string;
+	title: string;
+	excerpt: string;
+	updatedAt: string;
+}
+
+interface PostsProps {
+	posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
 	return (
 		<>
 			<Head>
@@ -10,25 +27,47 @@ export default function Posts() {
 
 			<main className={styles.container}>
 				<div className={styles.posts}>
-					<a href="#">
-						<time>19 de Abril de 2022</time>
-						<strong>Creating a new psots</strong>
-						<p>Description posts here!! 😎🥳</p>
-					</a>
+					{ posts.map(post => (
+						<a href="#" key={post.slug}>
+							<time>{post.updatedAt}</time>
+							<strong>{post.title}</strong>
+							<p>{post.excerpt}</p>
+						</a>
+					))}
 
-					<a href="#">
-						<time>19 de Abril de 2022</time>
-						<strong>Creating a new psots</strong>
-						<p>Description posts here!! 😎🥳</p>
-					</a>
-
-					<a href="#">
-						<time>19 de Abril de 2022</time>
-						<strong>Creating a new psots</strong>
-						<p>Description posts here!! 😎🥳</p>
-					</a>
 				</div>
 			</main>
 		</>
 	)
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+	const prismic = getPrismicClient()
+
+	const response = await prismic.query([
+		Prismic.predicates.at('document.type', 'publication-id')
+	], {
+		fetch: ['publication-id.Title', 'publication-id.Content'],
+		pageSize: 100,
+	})
+
+	const posts = response.results.map(post => {
+		return {
+			slug: post.uid,
+			title: RichText.asText(post.data.title),
+			excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+			updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+				day: '2-digit',
+				month: 'long',
+				year: 'numeric'
+			})
+		}
+	})
+
+	console.log(posts)
+	return {
+		props: {
+			posts
+		}
+	}
 }
